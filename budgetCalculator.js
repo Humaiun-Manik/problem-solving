19d9a092c55f9c32ac1162e2ff0e47c3131e1457
Humaiun-Manik <humaiunkobirr@gmail.com>
2021-12-13 02:49:08 +0600

    error handle

--- budgetCalculator.js
+++ budgetCalculator.js
@@ -3,12 +3,20 @@ function paperRequirments(humayun, munir, jhankar) {
     const munirEachBook = 200;
     const jhankarEachBook = 300;
 
-    const humayunPageNumber = humayunEachBook * humayun;
-    const munirPageNumber = munirEachBook * munir;
-    const jhankarPageNumber = jhankarEachBook * jhankar;
+    if (humayun < 0 || munir < 0 || jhankar < 0) {
+        return 'Nagetive number is not valid';
+    }
+    else if (humayun % 1 !== 0 || munir % 1 !== 0 || jhankar % 1 !== 0) {
+        return 'Number is not valid';
+    }
+    else {
+        const humayunPageNumber = humayunEachBook * humayun;
+        const munirPageNumber = munirEachBook * munir;
+        const jhankarPageNumber = jhankarEachBook * jhankar;
 
-    const totalPages = humayunPageNumber + munirPageNumber + jhankarPageNumber;
-    return totalPages;
+        const totalPages = humayunPageNumber + munirPageNumber + jhankarPageNumber;
+        return totalPages;
+    }
 }
 const result = paperRequirments(2, 1, 4);
 console.log(result);
